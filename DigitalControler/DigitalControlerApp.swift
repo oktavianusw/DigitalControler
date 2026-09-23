e251d87ee76b37bed85f2b3b1917c0d023f5a37d
waluya juang <waluyajuang330@gmail.com>
2026-09-23 21:42:18 +0700

    Initial Commit

---
+++ DigitalControler/DigitalControlerApp.swift
@@ -0,0 +1,32 @@
+//
+//  DigitalControlerApp.swift
+//  DigitalControler
+//
+//  Created by Jua on 23/09/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DigitalControlerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
